bc84f27d699bde79fbb3d14555b981dce6b03ebc
Chris Westerman <chris.westerman8@gmail.com>
2020-11-18 22:38:03 +0000

    initial commit

--- tests/lib_test.py
+++ tests/lib_test.py
@@ -5,15 +5,15 @@ import os
 import nflbigdata
 import pandas as pd
 # Import from our lib
-from nflbigdata.lib import clean_data
+# from nflbigdata.lib import clean_data
 import pytest
 
 
 def test_clean_data():
-    datapath = os.path.dirname(os.path.abspath(nflbigdata.__file__)) + '/data'
-    df = pd.read_csv('{}/data.csv.gz'.format(datapath))
-    first_cols = ['id', 'civility', 'birthdate', 'city', 'postal_code', 'vote_1']
-    assert list(df.columns)[:6] == first_cols
-    assert df.shape == (999, 142)
-    out = clean_data(df)
-    assert out.shape == (985, 119)
+    # datapath = os.path.dirname(os.path.abspath(nflbigdata.__file__)) + '/data'
+    # df = pd.read_csv('{}/data.csv.gz'.format(datapath))
+    # first_cols = ['id', 'civility', 'birthdate', 'city', 'postal_code', 'vote_1']
+    # assert list(df.columns)[:6] == first_cols
+    # assert df.shape == (999, 142)
+    # out = clean_data(df)
+    # assert out.shape == (985, 119)
